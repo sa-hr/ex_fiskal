e12c443a21af3fc9cb855f1f04c59993e135ee4e
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2024-12-28 15:15:58 +0100

    Release to hex.pm

--- mix.exs
+++ mix.exs
@@ -7,6 +7,10 @@ defmodule ExFiskal.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      name: "ExFiskal",
+      source_url: "https://github.com/sa-hr/ex_fiskal",
       deps: deps()
     ]
   end
@@ -19,7 +23,20 @@ defmodule ExFiskal.MixProject do
 
   defp deps do
     [
-      {:uuid, "~> 1.1"}
+      {:uuid, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Library for creating fiscalized receiptes as per Croatian fiscalization"
+  end
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sa-hr/ex_fiskal"}
     ]
   end
 end
