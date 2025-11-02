80c560532e31760590537bee1ce1b345499396de
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2025-11-02 09:46:15 +0100

    Bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExFiskal.MixProject do
   def project do
     [
       app: :ex_fiskal,
-      version: "0.1.3",
+      version: "0.2.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: description(),
